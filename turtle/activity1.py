738d6f9b3b9f72e6c945b1ce519153037e65fade
ashirwadsajith1-crypto <ashirwadsajith1@gmail.com>
2026-06-24 15:53:52 +0400

    function activity 1

---
+++ turtle/activity1.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.Screen().bgcolor("yellow")
+turtle.Screen().setup(500,300)
+polygon=turtle.Turtle()
+sidelength=5
+number_of_sides=4
+angle=360/number_of_sides
+for i in range(number_of_sides):
+    polygon.forward(sidelength)
+    polygon.right(angle)
+turtle.done()
+
