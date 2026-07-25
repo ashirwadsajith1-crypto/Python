5596afcbced19e90df277cb8165b996cd1a05ee6
ashirwadsajith1-crypto <ashirwadsajith1@gmail.com>
2026-07-25 10:25:15 +0400

    mathoperation

---
+++ RandomandMathModule/mathoperations.py
@@ -0,0 +1,14 @@
+import math
+a=math.ceil(23.56)
+b=math.floor(23.56)
+print(f"The smallest whole number less than or equal to the number is {a} and the largest number greater than or euqal to the number is {b}")
+x=10
+y=-15
+c=math.copysign(x,y)
+print(f"The copysign of two numbers is {c}")
+d=math.fabs(-96)
+e=math.fabs(56)
+print(f"The abs value of first number and second number is {d} and {e} respectively")
+f=int(math.gcd(24,56))
+print(f"The greatest common divisor of 24 and 56 is {f}")
+
