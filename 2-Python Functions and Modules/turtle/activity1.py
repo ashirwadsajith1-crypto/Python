1673be0ac0e6a2cb58315aca42133f7efb608f3c
ashirwadsajith1-crypto <ashirwadsajith1@gmail.com>
2026-07-31 09:25:45 +0400

    square.py

--- 2-Python Functions and Modules/turtle/activity1.py
+++ 2-Python Functions and Modules/turtle/activity1.py
@@ -2,8 +2,8 @@ import turtle
 turtle.Screen().bgcolor("yellow")
 turtle.Screen().setup(500,300)
 polygon=turtle.Turtle()
-sidelength=5
-number_of_sides=4
+sidelength=0.1
+number_of_sides=10**3
 angle=360/number_of_sides
 for i in range(number_of_sides):
     polygon.forward(sidelength)
