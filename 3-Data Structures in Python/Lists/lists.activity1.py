e15aae2a9c5f75f8b477631d1a94f6a88820e705
ashirwadsajith1-crypto <ashirwadsajith1@gmail.com>
2026-08-05 15:55:31 +0400

    introtoists.

---
+++ 3-Data Structures in Python/Lists/lists.activity1.py
@@ -0,0 +1,9 @@
+emptylist=[]
+print(emptylist)
+numberlist=[1,11,21,31,41]
+print(f"The numbers in the list are: {numberlist}")
+triples=[1,2,3]*3
+print(triples)
+listhunderds=[100,200,300,400,500]
+print(listhunderds[::-1])
+
