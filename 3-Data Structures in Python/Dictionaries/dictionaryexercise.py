48a31f1ca0e03396656cdc87ca99ddf5c67c05e7
ashirwadsajith1-crypto <ashirwadsajith1@gmail.com>
2026-08-19 16:04:48 +0400

    dictoionar exericse

---
+++ 3-Data Structures in Python/Dictionaries/dictionaryexercise.py
@@ -0,0 +1,10 @@
+favourites={"chocolate" : "Dairy Milk", "color":"blue" ,"sport":"cricket"}
+print(favourites," ", len(favourites))
+favourites['food']="dosa"
+print(favourites)
+favourites['color']="orange"
+print(favourites['color'])
+favourites.pop("chocolate")
+print(favourites)
+favourites.clear()
+print(favourites)
